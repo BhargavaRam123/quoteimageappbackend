c62f14e81b0c0494ae5e7cdc1af3996d0b4f50f3
BhargavaRam123 <allabhargavaram200@gmail.com>
2024-05-30 22:47:17 +0530

    cors

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ dotenv.config({
 });
 const app = express();
 const port = process.env.PORT
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 
 app.use(
   express.json({
